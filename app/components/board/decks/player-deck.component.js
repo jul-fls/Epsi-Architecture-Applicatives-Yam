5f2084fefe26e4363f69b3d06e85331b44cecdb9
Sangmin SHIM <tkdals4723@gmail.com>
2024-04-23 18:32:11 +0200

    Update player-deck.component.js
    added condition to deactiavte Roll button

--- app/components/board/decks/player-deck.component.js
+++ app/components/board/decks/player-deck.component.js
@@ -62,7 +62,7 @@ const PlayerDeck = () => {
             ))}
           </View>
 
-          {displayRollButton && (
+          {displayRollButton && rollsCounter < rollsMaximum && (
             <>
               <TouchableOpacity style={styles.rollButton} onPress={rollDices}>
                 <Text style={styles.rollButtonText}>Roll</Text>
