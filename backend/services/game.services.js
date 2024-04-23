ec1d0de5c7dcb10174a13264b48a7776d740434f
Sangmin SHIM <tkdals4723@gmail.com>
2024-04-23 18:31:08 +0200

    bug fixed Roll Button hidden just after 3 times of roll dice

--- backend/services/game.services.js
+++ backend/services/game.services.js
@@ -32,12 +32,12 @@ const CHOICES_INIT = {
 };
 
 const ALL_COMBINATIONS = [
-  { value: "Brelan1", id: "brelan1" },
-  { value: "Brelan2", id: "brelan2" },
-  { value: "Brelan3", id: "brelan3" },
-  { value: "Brelan4", id: "brelan4" },
-  { value: "Brelan5", id: "brelan5" },
-  { value: "Brelan6", id: "brelan6" },
+  { value: "Brelan 1", id: "brelan1" },
+  { value: "Brelan 2", id: "brelan2" },
+  { value: "Brelan 3", id: "brelan3" },
+  { value: "Brelan 4", id: "brelan4" },
+  { value: "Brelan 5", id: "brelan5" },
+  { value: "Brelan 6", id: "brelan6" },
   { value: "Full", id: "full" },
   { value: "Carré", id: "carre" },
   { value: "Yam", id: "yam" },
