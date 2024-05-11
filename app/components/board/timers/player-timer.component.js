79b4512faad4a31a7c8e327b521efb42c2edb8a0
Sangmin SHIM <tkdals4723@gmail.com>
2024-05-11 16:46:31 +0200

    timer on progress

--- app/components/board/timers/player-timer.component.js
+++ app/components/board/timers/player-timer.component.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 import { SocketContext } from "../../../contexts/socket.context";
 import { COLOR } from "../../../constants/color";
@@ -16,7 +17,33 @@ const PlayerTimer = () => {
 
   return (
     <View style={styles.playerTimerContainer}>
-      <Text style={{ color: COLOR.WHITE }}>Timer : {playerTimer}</Text>
+      {playerTimer !== 0 ? (
+        <View>
+          <View style={{ marginTop: 5 }}>
+            <CountdownCircleTimer
+              isPlaying={playerTimer !== 0 ? true : false}
+              size={23}
+              strokeWidth={5}
+              trailStrokeWidth={5}
+              duration={30}
+              colors={[
+                COLOR.ZELDA_BLUE,
+                COLOR.GRAY,
+                COLOR.ZELDA_YELLOW,
+                COLOR.DARK_RED,
+              ]}
+              colorsTime={[10, 6, 3, 0]}
+              onComplete={() => ({
+                shouldRepeat: true,
+                delay: 1,
+              })}
+            />
+          </View>
+          <Text style={{ color: COLOR.WHITE }}>{playerTimer} ⏳</Text>
+        </View>
+      ) : (
+        <Text style={{ color: COLOR.WHITE }}>0 ⏳</Text>
+      )}
     </View>
   );
 };
